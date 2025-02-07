2c6bc804cc63a314b3835688bf95d3490adfda27
Andrew Jowe <andrewjowe@yahoo.com>
2025-02-06 16:56:58 -0800

    extensive testing

--- tests/model.py
+++ tests/model.py
@@ -2,14 +2,17 @@ import unittest
 from ..model import Model
 
 class MyTestCase(unittest.TestCase):
-  def test_polling(self):
+  def test_rag(self):
     model = Model()
     model.add_chunk_to_database("I like apples")
     model.add_chunk_to_database("I don't like oranges")
     model.add_chunk_to_database("I am ok with peaches")
-    results = model.retrieve("What do I think about peaches?")
 
+    results = model.retrieve("What do I think about peaches?")
     self.assertEqual(results[0][0], "I am ok with peaches")
+
+    results = model.retrieve("What do I not like?")
+    self.assertEqual(results[0][0], "I don't like oranges")
 
 if __name__ == '__main__':
   unittest.main()
