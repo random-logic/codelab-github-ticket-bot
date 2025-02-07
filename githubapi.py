469b82b6361fcb4822b59bed799e6b4f1dbcbc31
Andrew Jowe <andrewjowe@yahoo.com>
2025-02-06 17:54:01 -0800

    clean up

--- githubapi.py
+++ githubapi.py
@@ -12,15 +12,8 @@ class GitHub:
     self.repo = self.g.get_repo(repo_name)
 
   def post_github_comment(self, issue_number, comment):
-    url = f"https://api.github.com/repos/{self.repo_owner}/{self.repo_name}/issues/{issue_number}/comments"
-    headers = {
-      "Authorization": f"Bearer {self.account_token}" if self.account_token else "",
-      "Accept": "application/vnd.github.v3+json"
-    }
-    data = {"body": comment}
-
-    response = requests.post(url, headers=headers, json=data)
-    return response.json()
+    # TODO
+    raise NotImplementedError
 
   def poll(self):
     exclude_numbers = {1, 2} # 1 and 2 aren't legit opportunities
